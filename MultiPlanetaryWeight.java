ac7bf5ee510cc79f24eab9bb26712f9c1ad27217
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-09-30 23:56:49 +0300

    Update MultiPlanetaryWeight.java

--- MultiPlanetaryWeight.java
+++ MultiPlanetaryWeight.java
@@ -18,11 +18,27 @@ public class MultiPlanetaryWeight {
 	                planets,
 	                planets[0]);
 	        if (planet.equals("Mercury")){
+	        	System.out.println ("Your weight on Mercury"+ weight*0.38);
+	        }
+	        else if (planet.equals("Venus")){
+	        	System.out.println ("Your weight on Venus"+ weight*0.91);
+	        }
+	        else if (planet.equals("Mars")){
 	        	System.out.println ("Your weight on Mars"+ weight*0.38);
 	        }
-	        else {
-	        	System.out.println ("Your weight on Mars");
+	        else if (planet.equals("Jupiter")){
+	        	System.out.println ("Your weight on Jupiter"+ weight*2.36);
 	        }
+	        else if (planet.equals("Saturn")){
+	        	System.out.println ("Your weight on Saturn"+ weight*0.92);
+	        }
+	        else if (planet.equals("Uranus")){
+	        	System.out.println ("Your weight on Uranus"+ weight*0.89);
+	        }
+	        else if (planet.equals("Neptune")){
+	        	System.out.println ("Your weight on Neptune"+ weight*1.13);
+	        }
+	        
 	}
 
 }
