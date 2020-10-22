4246e732365610cb620ba0e797d7ca8db453f727
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-22 11:46:20 +0300

    Update Emirp.java

--- Emirp.java
+++ Emirp.java
@@ -3,7 +3,65 @@ package eddu.smg;
 public class Emirp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        int numbers = 1;
+         long numberNow =10;
+        		while(numbers<=121) {
+			if(prime(numberNow)&& emirp(numberNow)){
+			    if(numbers%10!=0){
+			    System.out.print(numberNow + " ");
+			    numbers++;}
+			    else{
+			        System.out.println(numberNow + " ");
+			    numbers++;
+			    }
+			}
+			numberNow++;
+		}
+
+	}
+	 public static boolean prime(long num) {
+			boolean isPrime = true;
+	     for(int i = 2; i <= num/2; ++i)
+	     {
+	         if(num % i == 0)
+	         {
+	             isPrime = false;
+	             break;
+	         }
+	     }
+
+	     if (isPrime) {
+	    	 return true;
+	     }
+	     else {
+	         return false;
+	 	}
+	 }
+	 
+	 public static boolean emirp(long n) {
+		 long r,sum=0,temp;    
+		  temp=n;   
+		  while(n>0){    
+		   r=n%10;    
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }
+			boolean emirp = true;
+	     for(int i = 2; i <= sum/2; ++i)
+	     {
+	         if(sum % i == 0)
+	         {
+	             emirp = false;
+	             break;
+	         }
+	     }
+
+	     if (emirp) {
+	    	 return true;
+	     }
+	     else {
+	         return false;
+	 	}
 
 	}
 
