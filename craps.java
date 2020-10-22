cc5f24aa7b66fbd0ecce8ec5629543d99fce9abc
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-22 17:30:34 +0300

    Update craps.java

--- craps.java
+++ craps.java
@@ -5,15 +5,38 @@ public class craps {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
-		int[]dice1 ={1,2,3,4,5,6};
-		int[]dice2 ={1,2,3,4,5,6};
-		int sum1, sum2;
+		int dice1, dice2;
+		int sum1=0, sum=0;
 	
-		sum1 = mix()+ mix();
-		
-		System.out.println (sum1);
-
+		dice1 = mix();
+		dice2 = mix();
+		sum1 = dice1 +dice2;
+		System.out.println ("You rolled "+ dice1+" and "+ dice2  );
+		System.out.println ("Your point is  "+ sum1  );
+		if(sum1==2 || sum1==3 || sum1==12){
+			System.out.println("You lose");
+			}
+		else if(sum1==7 || sum1 ==11){
+			System.out.println("You win");
+			
+		}
+		else{
+		while(sum!=7 ){
+			dice1 = mix();
+			dice2 = mix();
+			sum = dice1 +dice2;
+			System.out.println ("You rolled "+ dice1+" and "+ dice2  );
+			System.out.println ("Your point is  "+ sum  );
+			if(sum==sum1){
+				System.out.println("You win ");
+			}
+		}
+		if(sum==7){
+			System.out.println("You lose ");
+		}
+	  }
 	}
+
 	public static int mix(){
 		Random random = new Random();
 		int rand = 0;
