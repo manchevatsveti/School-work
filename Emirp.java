c97da89658dc3964fcd95709f9a49ae50119aeda
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-22 16:42:52 +0300

    Update Emirp.java

--- Emirp.java
+++ Emirp.java
@@ -1,68 +1,70 @@
 package eddu.smg;
 
-public class Emirp {
-
+public class emirp {
 	public static void main(String[] args) {
-		        int numbers = 1;
-         long numberNow =10;
-        		while(numbers<=121) {
-			if(prime(numberNow)&& emirp(numberNow)){
-			    if(numbers%10!=0){
-			    System.out.print(numberNow + " ");
-			    numbers++;}
-			    else{
-			        System.out.println(numberNow + " ");
-			    numbers++;
-			    }
-			}
-			numberNow++;
-		}
-
+        int numbers = 1;
+        long numberNow =10;
+		while(numbers<=121) {
+	if( prime(numberNow) && emirp(numberNow)){
+	    if(numbers%10!=0){
+	    System.out.print(numberNow + " ");
+	    numbers++;}
+	    else{
+	        System.out.println(numberNow + " ");
+	    numbers++;
+	    }
 	}
-	 public static boolean prime(long num) {
-			boolean isPrime = true;
-	     for(int i = 2; i <= num/2; ++i)
-	     {
-	         if(num % i == 0)
-	         {
-	             isPrime = false;
-	             break;
-	         }
-	     }
+	numberNow++;
+}
 
-	     if (isPrime) {
-	    	 return true;
-	     }
-	     else {
-	         return false;
-	 	}
-	 }
+}
+public static boolean prime(long num) {
+	boolean isPrime = true;
+ for(int i = 2; i <= num/2; ++i)
+ {
+     if(num % i == 0)
+     {
+         isPrime = false;
+         break;
+     }
+ }
+
+ if (isPrime) {
+	 return true;
+ }
+ else {
+     return false;
+	}
+}
+
+public static boolean emirp(long n) {
+ long r,sum=0,temp;    
+  temp=n;   
+  while(n>0){    
+   r=n%10;    
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }
+  boolean emirp = true;
+  if(sum!=temp){
+ for(int i = 2; i <= sum/2; ++i){
+     if(sum % i == 0)
+     {emirp = false; 
+     break;
+     }
 	 
-	 public static boolean emirp(long n) {
-		 long r,sum=0,temp;    
-		  temp=n;   
-		  while(n>0){    
-		   r=n%10;    
-		   sum=(sum*10)+r;    
-		   n=n/10;    
-		  }
-			boolean emirp = true;
-	     for(int i = 2; i <= sum/2; ++i)
-	     {
-	         if(sum % i == 0)
-	         {
-	             emirp = false;
-	             break;
-	         }
-	     }
+ }
+  }
+  else{
+	  emirp = false;
+  }
 
-	     if (emirp) {
-	    	 return true;
-	     }
-	     else {
-	         return false;
-	 	}
-
+ if (emirp) {
+	 return true;
+ }
+ else {
+     return false;
 	}
 
+}
 }
