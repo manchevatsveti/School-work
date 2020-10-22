0bbbb67d64bb76358d0504d70e94621253eb10a5
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-10-22 11:14:59 +0300

    Update Palindromic_prime.java

--- Palindromic_prime.java
+++ Palindromic_prime.java
@@ -4,15 +4,15 @@ public class Palindromic_prime {
 
 	public static void main(String[] args) {
 		
-		int numbers=0;
-		int numberNow =2;
-		while(numbers<=120) {
+		int numbers=1;
+		long numberNow =2;
+		while(numbers<=121) {
 			if(prime(numberNow) && palindromic(numberNow)){
 				if(numbers%10!=0) {
 				System.out.print(numberNow + " ");
 				numbers++;
 				}
-				else if (numbers%10==0){
+				else if (numbers%10==0 ){
 					System.out.print(numberNow + " \n");
 					numbers++;
 				}
@@ -22,7 +22,7 @@ public class Palindromic_prime {
 	
 
 	}
- public static boolean prime(int num) {
+ public static boolean prime(long num) {
 		boolean isPrime = true;
      for(int i = 2; i <= num/2; ++i)
      {
@@ -41,8 +41,8 @@ public class Palindromic_prime {
  	}
  }
  
- public static boolean palindromic(int n) {
-	 int r,sum=0,temp;    
+ public static boolean palindromic(long n) {
+	 long r,sum=0,temp;    
 	  temp=n;   
 	  while(n>0){    
 	   r=n%10;    
@@ -57,3 +57,4 @@ public class Palindromic_prime {
 	  }
  	}
 }
+
